618d5387c6ed8574a12a7cf45f6a6d7f8ff324aa
am999 <am9999072080@gmail.com>
2023-03-10 20:26:05 +0300

    Мастер

---
+++ src/Class1.java
@@ -0,0 +1,13 @@
+public class Class1 {
+    public static void main(String[]args){
+        Class2 person1=new Class2();
+        System.out.print("Меня зовут "+person1.name1);
+        person1.y(10);
+        Class2 person2=new Class2();
+        System.out.print("Меня зовут "+person2.name2);
+        person2.y(7);
+        Class2 person3=new Class2();
+        System.out.print("Меня зовут "+person3.name3);
+        person3.y(7);
+    }
+}
